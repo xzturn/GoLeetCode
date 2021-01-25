48ff075fb7954e37ca4203b10432cdfee8d1fb2d
Sun Peng <xzturn@outlook.com>
2021-01-26 00:25:10 +0800

    better 0006

--- 0006.zigZag.go
+++ 0006.zigZag.go
@@ -3,35 +3,34 @@ package GoLeetCode
 func convert(s string, numRows int) string {
     if (numRows == 1) { return s }
     
-    outter := func(s string, n, r, i, step int, buf *[]byte, cur int) int {
+    n, step, cur := len(s), (numRows - 1) * 2, 0
+    buf := make([]byte, n)
+    
+    outter := func(i int) {
         for i < n {
-            (*buf)[cur] = s[i]
+            buf[cur] = s[i]
             i += step
             cur++
         }
-        return cur
     }
     
-    inner := func(s string, n, r, i, step int, buf *[]byte, cur int) int {
-        delta := (r - i - 1) * 2
+    inner := func(i int) {
+        delta := (numRows - i - 1) * 2
         for i < n {
-            (*buf)[cur] = s[i]
+            buf[cur] = s[i]
             cur++
             if k := i + delta; k < n {
-                (*buf)[cur] = s[k]
+                buf[cur] = s[k]
                 cur++
             }
             i += step
         }
-        return cur
     }
     
-    n, step := len(s), (numRows - 1) * 2
-    buf := make([]byte, n)
-    cur := outter(s, n, numRows, 0, step, &buf, 0)
+    outter(0)
     for i := 1; i < numRows - 1; i++ {
-        cur = inner(s, n, numRows, i, step, &buf, cur)
+        inner(i)
     }
-    outter(s, n, numRows, numRows - 1, step, &buf, cur)
+    outter(numRows - 1)
     return string(buf)
 }
