232f4098937de95dd0ce5c42cdc1d84c6934fc5a
Sun Peng <xzturn@outlook.com>
2021-01-26 09:09:44 +0800

    fix typo

--- 0004.medianSortedArrays.go
+++ 0004.medianSortedArrays.go
@@ -1,6 +1,6 @@
 package GoLeetCode
 
-func findMedianSortedArrays(nums1 []int, nums2 int[]) float64 {
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     n1, n2 := len(nums1), len(nums2)
     if n1 > n2 {
         return findMedianSortedArrays(nums2, nums1)
@@ -21,16 +21,15 @@ func findMedianSortedArrays(nums1 []int, nums2 int[]) float64 {
         }
     }
 
-    max := func(x, y int) int { if x > y { return x } else { return y } }
-    min := func(x, y int) int { if x < y { return x } else { return y } }
-
     left, right := 0, 0
     if mid1 == 0 {
         left = nums2[mid2 - 1]
     } else if mid2 == 0 {
         left = nums1[mid1 - 1]
+    } else if nums1[mid1 - 1] > nums2[mid2 - 1] {
+        left = nums1[mid1 -1]
     } else {
-        left = max(nums1[mid1 -1], nums2[mid2 - 1])
+        left = nums2[mid2 - 1]
     }
     if (n1 + n2) & 1 == 1 {
         return float64(left)
@@ -40,8 +39,41 @@ func findMedianSortedArrays(nums1 []int, nums2 int[]) float64 {
         right = nums2[mid2]
     } else if mid2 == n2 {
         right = nums1[mid1]
+    } else if nums1[mid1] < nums2[mid2] {
+        right = nums1[mid1]
     } else {
-        right = min(nums1[mid1], nums2[mid2])
+        right = nums2[mid2]
     }
     return float64(left + right) / 2
+}
+
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+    i1, i2, n1, n2 := 0, 0, len(nums1), len(nums2)
+    i, n := 0, n1 + n2
+    nums := make([]int, n)
+    for i1 < n1 && i2 < n2 {
+        if nums1[i1] < nums2[i2] {
+            nums[i] = nums1[i1]
+            i1++
+        } else {
+            nums[i] = nums2[i2]
+            i2++
+        }
+        i++
+    }
+    for i1 < n1 {
+        nums[i] = nums1[i1]
+        i++
+        i1++
+    }
+    for i2 < n2 {
+        nums[i] = nums2[i2]
+        i++
+        i2++
+    }
+    k := n / 2
+    if n % 2 == 1 {
+        return float64(nums[k])
+    }
+    return float64(nums[k] + nums[k-1]) / 2
 }
--- 0005.longestPalindrome.go
+++ 0005.longestPalindrome.go
@@ -1,18 +1,16 @@
 package GoLeetCode
 
 func longestPalindrome(s string) string {
-    palindrome := func(s string, n, l, r int) (int, int, int) {
-        for l >= 0 && r < n  && s[l] == s[r] {
-            l--
-            r++
-        }
+    n := len(s)
+    palindrome := func(l, r int) (int, int, int) {
+        for l >= 0 && r < n  && s[l] == s[r] { l--; r++ }
         return l+1, r, r-l-1
     }
     
-    n, l, r, k := len(s), 0, 0, 0
+    l, r, k := 0, 0, 0
     for i := 0; i < n; i++ {
-        l1, r1, k1 := palindrome(s, n, i, i)
-        l2, r2, k2 := palindrome(s, n, i, i+1)
+        l1, r1, k1 := palindrome(i, i)
+        l2, r2, k2 := palindrome(i, i+1)
         if k < k1 { l, r, k = l1, r1, k1 }
         if k < k2 { l, r, k = l2, r2, k2 }
     }
